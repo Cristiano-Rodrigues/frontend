9eb2a1cd9c0974ce1197273a75b72771ca518eba
Cristiano-Rodrigues <realchrisvibez@gmail.com>
2024-02-10 15:24:21 +0100

    refactor: use moment to format date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import moment from 'moment';
+import 'moment/locale/pt'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export function formatDate (date: Date, format: string) {
-  const monthNames = (
-    'Janeiro,Fevereiro,Março,Abril,Maio,Junho,Julho,Agosto,Setembro,Outubro,Novembro,Dezembro'
-    .split(',')
-  )
-  return format
-    .replaceAll('YYYY', date.getFullYear().toString())
-    .replaceAll('MMMM', monthNames[date.getMonth()])
-    .replaceAll('DD', date.getDate().toString())
+  moment.locale('pt')
+  return moment(date).format(format)
 }
