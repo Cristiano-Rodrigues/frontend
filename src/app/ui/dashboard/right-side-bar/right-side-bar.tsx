e7c564f89d26a6cd2a5243493ca64580386b51c2
Cristiano-Rodrigues <realchrisvibez@gmail.com>
2024-02-10 12:00:29 +0100

    feat: make callendar work

---
+++ src/app/ui/dashboard/right-side-bar/right-side-bar.tsx
@@ -0,0 +1,43 @@
+import { formatDate } from "@/lib/utils";
+import { Calendar } from "./calendar";
+
+const InfoCard = ({
+  title,
+  amount
+}: {
+  title: string;
+  amount: number;
+}) => {
+  return (
+    <div className="flex flex-col justify-between gap-6 w-1/2 px-4 py-6 bg-white rounded-md">
+      <p className="text-xs text-darkGray">{ title }</p>
+      <p className="text-2xl text-primary-100">{ amount },00<span>Kz</span></p>
+    </div>
+  )
+}
+
+export function RightSideBar () {
+  const date = new Date()
+  return (
+    <div className="flex flex-col gap-4 w-full md:w-[420px] shrink-0 p-4 bg-lightestGray">
+      <div className="flex flex-col gap-2">
+        <h4 className="font-bold">Resultados das vendas <span className="ml-1 font-normal text-xs text-darkGray">Este mês</span></h4>
+        <div className="flex gap-2">
+          <InfoCard title="Total de vendas semanais" amount={0} />
+          <InfoCard title="Total de vendas mensais" amount={0} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h4 className="font-bold">Hoje <span className="ml-1 font-normal text-xs text-darkGray">{
+          formatDate(date, 'DD de MMMM de YYYY')
+        }</span></h4>
+        <Calendar />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h4 className="font-bold">Eventos <span className="ml-1 font-normal text-xs text-darkGray">{
+          formatDate(date, 'MMMM de YYYY')
+        }</span></h4>
+      </div>
+    </div>
+  )
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate (date: Date, format: string) {
+  const monthNames = (
+    'Janeiro,Fevereiro,Março,Abril,Maio,Junho,Julho,Agosto,Setembro,Outubro,Novembro,Dezembro'
+    .split(',')
+  )
+  return format
+    .replaceAll('YYYY', date.getFullYear().toString())
+    .replaceAll('MMMM', monthNames[date.getMonth()])
+    .replaceAll('DD', date.getDate().toString())
+}
